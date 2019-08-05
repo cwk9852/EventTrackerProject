da43ee23c478e6914837034d1b9db8673980dfb3
cwk9852 <cecil.kitch@gmail.com>
2019-08-05 08:48:05 -0600

    Configure WAR

--- OutbreakTracker/src/main/java/com/skilldistillery/eventtracker/OutbreakTrackerApplication.java
+++ OutbreakTracker/src/main/java/com/skilldistillery/eventtracker/OutbreakTrackerApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.eventtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OutbreakTrackerApplication {
+public class OutbreakTrackerApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OutbreakTrackerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OutbreakTrackerApplication.class, args);
